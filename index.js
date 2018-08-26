358f42ac79fa2ea5236f5aeba5c77a9b450504bf
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-26 21:44:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
-  const newObj = destructivelyUpdateDriverWithKeyAndValue(driver, key, value);
 }
 
 function deleteFromDriverByKey(driver, key) {
