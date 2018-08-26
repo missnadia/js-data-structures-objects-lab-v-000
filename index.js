2f3014889d821054ce83e4f59f833044b8babf9b
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-26 21:41:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
 
-function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
-
   return driver;
 }
 
